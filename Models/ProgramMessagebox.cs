37295de564198bec122863204865cd80135336f5
Prowadzacy <krzysztof@szwarc.net.pl>
2017-06-15 22:35:49 +0200

    Refactor

--- Models/ProgramMessagebox.cs
+++ Models/ProgramMessagebox.cs
@@ -5,22 +5,21 @@ namespace Maszyna.Models
 {
     class ProgramMessageBox
     {
+        private static String ProgramName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+
         public static void ShowError(String text)
         {
-            MessageBox.Show(text, System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show(text, ProgramName, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         public static void ShowInfo(String text)
         {
-            MessageBox.Show(text, System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
-                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(text, ProgramName, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         public static DialogResult ShowQuestion(String text)
         {
-            return MessageBox.Show(text, System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
-                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return MessageBox.Show(text, ProgramName, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
         }
     }
 }
